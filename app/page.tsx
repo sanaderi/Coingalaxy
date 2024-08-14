3d8215d20ecdfe4a8a8cd77424b978343ef0a194
Sadeq <sadeq.naderi@gmail.com>
2024-08-14 16:26:52 +0330

    Fix compile error

--- app/page.tsx
+++ app/page.tsx
@@ -163,7 +163,7 @@ export default function Home() {
                         onChange={(event) =>
                           setFormValues({
                             ...formValues,
-                            value: event.target.value
+                            value: Number(event.target.value)
                           })
                         }
                         className="mt-1 block w-full text-black px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
