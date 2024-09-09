1100c88ee224cc5c0c1eafc00d357dfeed92e5d7
Sadeq <sadeq.naderi@gmail.com>
2024-09-09 20:57:11 +0330

    Enable check requested ip address to receiveSignal

--- app/api/receiveSignal/route.ts
+++ app/api/receiveSignal/route.ts
@@ -3,6 +3,29 @@ import clientPromise from '../../../lib/mongodb'
 
 export async function POST(request: NextRequest) {
   try {
+    // Define the allowed IP addresses
+    const allowedIPs = [
+      // '::1',//For allow localhost
+      '52.89.214.238',
+      '34.212.75.30',
+      '54.218.53.128',
+      '52.32.178.7'
+    ]
+
+    // Get the IP address from the request headers
+    const requestIP = request.headers.get('x-forwarded-for') || request.ip || ''
+
+    // If there are multiple IPs (proxy), take the first one
+    const ip = requestIP.split(',')[0].trim()
+
+    // Check if the request IP matches any of the allowed IPs
+    if (!allowedIPs.includes(ip)) {
+      return NextResponse.json(
+        { error: 'Access denied: Your IP is not allowed.' },
+        { status: 403 }
+      )
+    }
+
     // Parse the incoming request body as JSON
     const body = await request.json()
 
