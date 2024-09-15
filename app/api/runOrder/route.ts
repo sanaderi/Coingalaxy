356750c3c7f2ab2de9a6d3259adadbc1a92b0090
Sadeq <sadeq.naderi@gmail.com>
2024-09-15 16:32:54 +0330

    Increase max timeout for runOrder

--- app/api/runOrder/route.ts
+++ app/api/runOrder/route.ts
@@ -1,3 +1,5 @@
+export const maxDuration = 60
+
 import { NextRequest, NextResponse } from 'next/server'
 import clientPromise from '../../../lib/mongodb'
 import { jupiterSwap } from '@/lib/jupiter'
