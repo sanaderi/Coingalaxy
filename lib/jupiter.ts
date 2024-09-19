012ccdae72f768eb372cdd1e9f2209cde6f9922f
Sadeq <sadeq.naderi@gmail.com>
2024-09-19 18:54:49 +0330

    Enable dynamic slippage

--- lib/jupiter.ts
+++ lib/jupiter.ts
@@ -73,7 +73,9 @@ export const jupiterSwap = async (
           // user public key to be used for the swap
           userPublicKey: quoteRequest.userPublicKey,
           // auto wrap and unwrap SOL. default is true
-          wrapAndUnwrapSol: true
+          wrapAndUnwrapSol: true,
+          dynamicSlippage: { maxBps: 150 }
+
           // feeAccount is optional. Use if you want to charge a fee.  feeBps must have been passed in /quote API.
           // feeAccount: "fee_account_public_key"
         })
