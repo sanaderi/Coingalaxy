907ed75b11fdd9019b74de9f59705a927aa4e166
Sadeq <sadeq.naderi@gmail.com>
2024-09-23 22:46:42 +0330

    Enable detect high/low on price

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -1,8 +1,43 @@
-import { fetchJupiterPrice } from "@/lib/jupiter";
-import { NextRequest } from "next/server";
+import { fetchJupiterPrice } from '@/lib/jupiter'
+import clientPromise from '@/lib/mongodb'
+import { NextRequest, NextResponse } from 'next/server'
 
-export async function POST(request:NextRequest) {
+export async function GET(request: NextRequest) {
+  try {
     const data = await fetchJupiterPrice('JUP')
-    const JupPrice = data.data.JUP.price;
-    console.log(JupPrice)
+    const jupPrice = data.data.JUP.price
+    const client = await clientPromise
+    const db = client.db('coingalaxy')
+    const settingCollection = db.collection('setting')
+
+    let highPrice = 0
+    let lowPrice = 0
+    let trend = ''
+    const setting = await settingCollection.findOne({}, { sort: { _id: 1 } })
+
+    if (setting) {
+      highPrice = setting.high
+      lowPrice = setting.low
+      trend = setting.trend
+      //Update latest high and low price
+      if (jupPrice > setting.high) highPrice = jupPrice
+      else if (jupPrice < setting.low) lowPrice = jupPrice
+      //End update latest high and low price
+    }
+    // Upsert: update the pbKey and pvKey if wallet exists, or insert if it doesn't
+    const result = await settingCollection.updateOne(
+      { key: 'price' }, // Filter: match by wallet address
+      {
+        $set: {
+          high: highPrice,
+          low: lowPrice,
+          trend: 'sell'
+        }
+      },
+      { upsert: true } // Create a new document if no match is found
+    )
+    return NextResponse.json({ status: 1, message: 'success' })
+  } catch {
+    return NextResponse.json({ message: 'error' })
+  }
 }
