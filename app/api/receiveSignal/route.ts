31d08d4b4a1141df00e7e79a9e79f94f2f6e5755
Sadeq <sadeq.naderi@gmail.com>
2024-10-20 12:22:15 +0330

    Enable get method for receive signal route

--- app/api/receiveSignal/route.ts
+++ app/api/receiveSignal/route.ts
@@ -1,7 +1,6 @@
 export const maxDuration = 300
 
 import { NextRequest, NextResponse } from 'next/server'
-import clientPromise from '../../../lib/mongodb'
 import { jupiterSwap } from '@/lib/jupiter'
 import { kv } from '@vercel/kv'
 
@@ -76,7 +75,73 @@ export async function POST(request: NextRequest) {
         console.info('zigzag signal sell')
       }
       await kv.set('zigzag', body.type)
-    } else if (fgh === 'buy' && zigzag == 'buy' && current_postion == 'sell') {
+    } 
+
+    if (!address) throw new Error(`Address incorrect`)
+
+    if (runSwap) {
+      const result_swap: string | undefined = await jupiterSwap(
+        sourceToken,
+        destinationToken,
+        address
+      )
+      if (result_swap === 'success')
+        await kv.set('current_postion', current_postion)
+
+      return NextResponse.json({
+        message: 'Swaped run',
+        data: { msg: 'task run', ip, result: result_swap }
+      })
+    } else {
+      return NextResponse.json({
+        message: 'Condition incorrect'
+      })
+    }
+  } catch (error) {
+    return NextResponse.json({ error: 'Failed to save data' }, { status: 500 })
+  }
+}
+
+export async function GET(request:NextRequest) {
+ // Retrieve the inserted value (for verification)
+  try {
+    // Define the allowed IP addresses
+    const allowedIPs = [
+      // '::1',//For allow localhost
+      '52.89.214.238',
+      '34.212.75.30',
+      '54.218.53.128',
+      '52.32.178.7'
+    ]
+
+    // Get the IP address from the request headers
+    const requestIP = request.headers.get('x-forwarded-for') || request.ip || ''
+
+    // If there are multiple IPs (proxy), take the first one
+    const ip = requestIP.split(',')[0].trim()
+
+    // Check if the request IP matches any of the allowed IPs
+    // if (!allowedIPs.includes(ip)) {
+    //   return NextResponse.json(
+    //     { error: 'Access denied: Your IP is not allowed.' },
+    //     { status: 403 }
+    //   )
+    // }
+
+
+    const usdcToken = 'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v'
+    const jupToken = 'JUPyiwrYJFskUPiHa7hkeR8VUtAeFoSYbKedZNsDvCN'
+
+    const address: Array<number> | null = await kv.get('address')
+    const zigzag = await kv.get('zigzag')
+    const fgh = await kv.get('fgh')
+    let current_postion = await kv.get('current_position')
+    
+
+    let sourceToken = ''
+    let destinationToken = ''
+    let runSwap = false
+    if (fgh === 'buy' && zigzag == 'buy' && current_postion == 'sell') {
       sourceToken = usdcToken
       destinationToken = jupToken
       runSwap = true
@@ -110,10 +175,11 @@ export async function POST(request: NextRequest) {
       })
     } else {
       return NextResponse.json({
-        message: 'Condition incorrect'
+        message: 'Condition incorrect'+current_postion
       })
     }
   } catch (error) {
-    return NextResponse.json({ error: 'Failed to save data' }, { status: 500 })
+    return NextResponse.json({ error: error }, { status: 500 })
   }
 }
+
