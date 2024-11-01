4db7c06b059cc785558405cac3da4b2b321b65c8
Sadeq <sadeq.naderi@gmail.com>
2024-11-01 23:44:26 +0330

    Disable retry in trade bot

--- app/api/receiveSignal/route.ts
+++ app/api/receiveSignal/route.ts
@@ -154,119 +154,119 @@ export async function POST(request: NextRequest) {
   }
 }
 
-export async function GET(request: NextRequest) {
+// export async function GET(request: NextRequest) {
  
-  // Retrieve the inserted value (for verification)
-  try {
-    // Define the allowed IP addresses
-    const allowedIPs = [
-      // '::1',//For allow localhost
-      '52.89.214.238',
-      '34.212.75.30',
-      '54.218.53.128',
-      '52.32.178.7'
-    ]
+//   // Retrieve the inserted value (for verification)
+//   try {
+//     // Define the allowed IP addresses
+//     const allowedIPs = [
+//       // '::1',//For allow localhost
+//       '52.89.214.238',
+//       '34.212.75.30',
+//       '54.218.53.128',
+//       '52.32.178.7'
+//     ]
 
-    // Get the IP address from the request headers
-    const requestIP = request.headers.get('x-forwarded-for') || request.ip || ''
+//     // Get the IP address from the request headers
+//     const requestIP = request.headers.get('x-forwarded-for') || request.ip || ''
 
-    // If there are multiple IPs (proxy), take the first one
-    const ip = requestIP.split(',')[0].trim()
+//     // If there are multiple IPs (proxy), take the first one
+//     const ip = requestIP.split(',')[0].trim()
 
-    // Check if the request IP matches any of the allowed IPs
-    // if (!allowedIPs.includes(ip)) {
-    //   return NextResponse.json(
-    //     { error: 'Access denied: Your IP is not allowed.' },
-    //     { status: 403 }
-    //   )
-    // }
+//     // Check if the request IP matches any of the allowed IPs
+//     // if (!allowedIPs.includes(ip)) {
+//     //   return NextResponse.json(
+//     //     { error: 'Access denied: Your IP is not allowed.' },
+//     //     { status: 403 }
+//     //   )
+//     // }
 
-    const usdcToken = 'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v'
-    const jupToken = 'JUPyiwrYJFskUPiHa7hkeR8VUtAeFoSYbKedZNsDvCN'
+//     const usdcToken = 'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v'
+//     const jupToken = 'JUPyiwrYJFskUPiHa7hkeR8VUtAeFoSYbKedZNsDvCN'
 
-    // const fgh = await kv.get('fgh')
-    console.log('Get fgh')
-    const fgh = await kv.get('fgh')
-    console.log('Get rsi')
-    const rsi = await kv.get('rsi')
-    console.log('Get position')
-    let current_position = await kv.get('current_position')
-    console.log('Get tp')
-    let tp_price = await kv.get('tp_price')
-    console.log('Get swap status')
-    let swap_inprocess = await kv.get('swap_inprocess')
-    console.log(`Start receive jup price`)
-    const priceData = await fetchJupiterPrice('JUP')
-    console.log(`Jup price received`)
+//     // const fgh = await kv.get('fgh')
+//     console.log('Get fgh')
+//     const fgh = await kv.get('fgh')
+//     console.log('Get rsi')
+//     const rsi = await kv.get('rsi')
+//     console.log('Get position')
+//     let current_position = await kv.get('current_position')
+//     console.log('Get tp')
+//     let tp_price = await kv.get('tp_price')
+//     console.log('Get swap status')
+//     let swap_inprocess = await kv.get('swap_inprocess')
+//     console.log(`Start receive jup price`)
+//     const priceData = await fetchJupiterPrice('JUP')
+//     console.log(`Jup price received`)
 
-    const jupPrice = priceData.data.JUP.price
-    console.log(`current jup pirce: ${jupPrice}`)
+//     const jupPrice = priceData.data.JUP.price
+//     console.log(`current jup pirce: ${jupPrice}`)
 
-    if (swap_inprocess) {
-      console.log('Already a Swap in progress')
-      return NextResponse.json({ msg: 'Swap in progress' })
-    }
-    await kv.set('swap_inprocess', true)
+//     if (swap_inprocess) {
+//       console.log('Already a Swap in progress')
+//       return NextResponse.json({ msg: 'Swap in progress' })
+//     }
+//     await kv.set('swap_inprocess', true)
 
-    let sourceToken = ''
-    let destinationToken = ''
-    let runSwap = false
-    if (rsi === 'buy' && current_position == 'sell') {
-      sourceToken = usdcToken
-      destinationToken = jupToken
-      runSwap = true
-      current_position = 'buy'
-      const tp_price = (jupPrice / 100) * 4 + jupPrice
-      console.log(`tp_price: ${tp_price}`)
-      await kv.set('tp_price', tp_price)
-      const sl_price = jupPrice - Number(jupPrice / 100) 
-      console.log(`sl_price: ${sl_price}`)
-      kv.set('sl_price',sl_price)
-      console.info(`retry to buyy`)
-    } else if (fgh === 'sell' && current_position == 'buy') {
-      sourceToken = jupToken
-      destinationToken = usdcToken
-      runSwap = true
-      current_position = 'sell'
-      console.info('retry to selll')
-    }
-    // else if (jupPrice >= Number(tp_price) && current_position == 'buy') {
-    //   sourceToken = jupToken
-    //   destinationToken = usdcToken
-    //   runSwap = true
-    //   current_position = 'sell'
-    //   await kv.set('zigzag', 'sell') //Simulate a HH or HL and force the bot to wait next signal
-    //   console.info(`tp price: ${tp_price}`)
-    //   console.info('retry to selll by tp method')
-    // }
-    console.log(`current_position: ${current_position}`)
+//     let sourceToken = ''
+//     let destinationToken = ''
+//     let runSwap = false
+//     if (rsi === 'buy' && current_position == 'sell') {
+//       sourceToken = usdcToken
+//       destinationToken = jupToken
+//       runSwap = true
+//       current_position = 'buy'
+//       const tp_price = (jupPrice / 100) * 4 + jupPrice
+//       console.log(`tp_price: ${tp_price}`)
+//       await kv.set('tp_price', tp_price)
+//       const sl_price = jupPrice - Number(jupPrice / 100) 
+//       console.log(`sl_price: ${sl_price}`)
+//       kv.set('sl_price',sl_price)
+//       console.info(`retry to buyy`)
+//     } else if (fgh === 'sell' && current_position == 'buy') {
+//       sourceToken = jupToken
+//       destinationToken = usdcToken
+//       runSwap = true
+//       current_position = 'sell'
+//       console.info('retry to selll')
+//     }
+//     // else if (jupPrice >= Number(tp_price) && current_position == 'buy') {
+//     //   sourceToken = jupToken
+//     //   destinationToken = usdcToken
+//     //   runSwap = true
+//     //   current_position = 'sell'
+//     //   await kv.set('zigzag', 'sell') //Simulate a HH or HL and force the bot to wait next signal
+//     //   console.info(`tp price: ${tp_price}`)
+//     //   console.info('retry to selll by tp method')
+//     // }
+//     console.log(`current_position: ${current_position}`)
 
-    if (!secretKey) throw new Error(`Address incorrect`)
+//     if (!secretKey) throw new Error(`Address incorrect`)
 
-    if (runSwap) {
-      const result_swap: string | undefined = await jupiterSwap(
-        sourceToken,
-        destinationToken,
-        secretKey
-      )
-      if (result_swap == 'success' || result_swap == 'insufficient_amount') {
-        await kv.set('current_position', current_position)
-        console.log(`successfuly position changed to ${current_position}`)
-      }
+//     if (runSwap) {
+//       const result_swap: string | undefined = await jupiterSwap(
+//         sourceToken,
+//         destinationToken,
+//         secretKey
+//       )
+//       if (result_swap == 'success' || result_swap == 'insufficient_amount') {
+//         await kv.set('current_position', current_position)
+//         console.log(`successfuly position changed to ${current_position}`)
+//       }
 
-      return NextResponse.json({
-        message: 'Swaped run',
-        data: { msg: 'task run', ip, result: result_swap }
-      })
-    } else {
-      return NextResponse.json({
-        message: 'Condition incorrect'
-      })
-    }
-  } catch (error) {
-    return NextResponse.json({ error: error }, { status: 500 })
-  } finally {
-    console.log('The Swap progress is over')
-    await kv.set('swap_inprocess', false)
-  }
-}
+//       return NextResponse.json({
+//         message: 'Swaped run',
+//         data: { msg: 'task run', ip, result: result_swap }
+//       })
+//     } else {
+//       return NextResponse.json({
+//         message: 'Condition incorrect'
+//       })
+//     }
+//   } catch (error) {
+//     return NextResponse.json({ error: error }, { status: 500 })
+//   } finally {
+//     console.log('The Swap progress is over')
+//     await kv.set('swap_inprocess', false)
+//   }
+// }
