5e4d817f90351be790c118aa03527e6be80adf3d
Sadeq Naderi <sadeq.naderi@gmail.com>
2025-05-03 18:40:47 +0330

    fix(mongo): disable mongo lib temprory

--- app/api/setKeys/route.ts
+++ app/api/setKeys/route.ts
@@ -1,36 +1,36 @@
-import { NextRequest, NextResponse } from 'next/server'
-import clientPromise from '../../../lib/mongodb'
+// import { NextRequest, NextResponse } from 'next/server'
+// import clientPromise from '../../../lib/mongodb'
 
-export async function POST(request: NextRequest) {
-  try {
-    // Parse the incoming request body as JSON
-    const body = await request.json()
+// export async function POST(request: NextRequest) {
+//   try {
+//     // Parse the incoming request body as JSON
+//     const body = await request.json()
 
-    // Validate the data (optional)
-    if (!body || !body.wallet || !body.pvKey || !body.pbKey) {
-      return NextResponse.json({ error: 'Invalid data' }, { status: 400 })
-    }
+//     // Validate the data (optional)
+//     if (!body || !body.wallet || !body.pvKey || !body.pbKey) {
+//       return NextResponse.json({ error: 'Invalid data' }, { status: 400 })
+//     }
 
-    // Connect to MongoDB
-    const client = await clientPromise
-    const db = client.db('coingalaxy')
-    const collection = db.collection('users')
+//     // Connect to MongoDB
+//     const client = await clientPromise
+//     const db = client.db('coingalaxy')
+//     const collection = db.collection('users')
 
-    // Upsert: update the pbKey and pvKey if wallet exists, or insert if it doesn't
-    const result = await collection.updateOne(
-      { wallet: body.wallet }, // Filter: match by wallet address
-      {
-        $set: {
-          pvKey: body.pvKey,
-          pbKey: body.pbKey
-        }
-      },
-      { upsert: true } // Create a new document if no match is found
-    )
+//     // Upsert: update the pbKey and pvKey if wallet exists, or insert if it doesn't
+//     const result = await collection.updateOne(
+//       { wallet: body.wallet }, // Filter: match by wallet address
+//       {
+//         $set: {
+//           pvKey: body.pvKey,
+//           pbKey: body.pbKey
+//         }
+//       },
+//       { upsert: true } // Create a new document if no match is found
+//     )
 
-    // Return a success response
-    return NextResponse.json({ message: 'Data saved successfully', result })
-  } catch (error) {
-    return NextResponse.json({ error: 'Failed to save data' }, { status: 500 })
-  }
-}
+//     // Return a success response
+//     return NextResponse.json({ message: 'Data saved successfully', result })
+//   } catch (error) {
+//     return NextResponse.json({ error: 'Failed to save data' }, { status: 500 })
+//   }
+// }
